8a2243be9c607ab5c47e1743a8553427adde2d91
Emmanuel <palinendir@gmail.comgit config --global user.name Emmanuelgit commit -m first>
2021-09-26 15:25:20 +0000

    first edit

--- script.js
+++ script.js
@@ -3,4 +3,24 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+
+
+let principal = document.getElementById("principal").value;
+// create a regular expression for validating principal input
+const reg = new RegExp('-d')
+function validatePrinciple(principal) {
+  if (principal === 0 || reg ) {
+    alert("Enter a positive number");
+    return false;
+  }
+}
+let rate = document.getElementById("rate").value;
+let years = document.getElementById("years").value;
+let interest = principal * years * rate /100;
+let year = new Date().getFullYear()+parseInt(years);
+function updateRate() 
+{
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+let result = document.getElementById("result").innerText=principal,rate,interest,year;
